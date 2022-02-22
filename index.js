1f2116e668b60f01da855551bd6ef7895e0d51a5
JoLesCode <jmaluleke63@gmail.com>
2022-02-22 14:30:46 +0200

    Updated create user page

--- index.js
+++ index.js
@@ -10,10 +10,12 @@ function createUser(){
     if(!document.getElementById("userc").value.toLowerCase().match(
         /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
     )){
-        alert("Email not in correct format");
-    }else if(!document.getElementById("passc").value.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/)){
         document.getElementById("userc").style.border = "thin solid red";
         document.getElementById("passc").style.border = "1px solid rgba(0,0,0,0.3)";
+        alert("Email not in correct format");
+    }else if(!document.getElementById("passc").value.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/)){
+        document.getElementById("passc").style.border = "thin solid red";
+        document.getElementById("userc").style.border = "1px solid rgba(0,0,0,0.3)";
         alert("Password needs to have at least 1 uppercase alphabet, 1 lowercase alphabet, 1 number, 1 special character, and must have a minimum of 8 characters");
     }else{
         user.push(document.getElementById("userc").value);
